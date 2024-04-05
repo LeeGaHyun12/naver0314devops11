470f355158004ec5cd355732966445c55061a6a0
GAHYUN <bearsi012@naver.com>
2024-04-05 17:40:18 +0900

    0405

---
+++ dbwork/0405.sql
@@ -0,0 +1,16 @@
+create table student
+(
+    num smallint auto_increment primary key ,
+    name varchar(20),
+    java smallint,
+    html smallint,
+    spring smallint,
+    testday datetime
+);
+
+insert into student values (null,'이효리',90,80,97,now());
+
+select num,name,java,html,spring,
+       java+html+spring tot, round((java+html+spring)/3,1) avg,
+       date_format(student.testday,'%Y-%m-%d %H:%i') testday
+from student;
